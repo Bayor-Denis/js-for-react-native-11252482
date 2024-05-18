1d9f58d8000371d1b651d9bc70b3729f42cd9cb2
Bayor-Denis <dbbayor@st.ug.edu.gh>
2024-05-18 00:12:43 +0000

    Work in progress

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -16,7 +16,33 @@ function processArray(arr) {
     });
 }
 
+/**
+ * Formats an array of strings based on corresponding numbers.
+ *
+ * @param {string[]} strArr - The array of strings to format.
+ * @param {number[]} numArr - The array of numbers processed by processArray.
+ * @returns {string[]} - The new array with formatted strings.
+ */
+function formatArrayStrings(strArr, numArr) {
+    return strArr.map((str, index) => {
+        if (index >= numArr.length) {
+            return str;
+        }
+        
+        if (numArr[index] % 2 === 0) {
+            return str.toUpperCase(); // Capitalize the entire string if the number is even
+        } else {
+            return str.toLowerCase(); // Convert the string to lowercase if the number is odd
+        }
+    });
+}
+
 
 const inputArray = [1, 2, 3, 4, 5, 6, 7, 8];
-const resultArray = processArray(inputArray);
-console.log(resultArray); 
+const processedNumbers = processArray(inputArray);
+console.log(processedNumbers); // Output: [3, 4, 9, 16, 15, 36, 21, 64]
+
+const stringArray = ["My", "name", "is", "Denis", "I", "am", "learning", "Javascript"];
+const formattedStrings = formatArrayStrings(stringArray, processedNumbers);
+console.log(formattedStrings); // Output: [ 'my', 'NAME', 'is', 'DENIS', 'i', 'AM', 'learning', 'JAVASCRIPT' ]
+
